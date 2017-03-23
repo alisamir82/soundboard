4208d6ef3a560e94b910dbcddaea5cd6377106fc
Ali Said <alisamir82@gmail.com>
2017-03-23 22:37:02 +0000

    Initial setup

---
+++ SoundBoard/recorderViewController.swift
@@ -0,0 +1,31 @@
+//
+//  recorderViewController.swift
+//  SoundBoard
+//
+//  Created by Ali Said on 23/03/2017.
+//  Copyright © 2017 compume. All rights reserved.
+//
+
+import UIKit
+
+class recorderViewController: UIViewController {
+
+    
+    @IBOutlet var recordButton: UIView!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+}
