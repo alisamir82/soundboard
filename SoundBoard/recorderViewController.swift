84a0b819d73bf5925e4f6695a5335a701eefb935
Ali Said <alisamir82@gmail.com>
2017-03-23 23:04:40 +0000

    soundRecorder setting is done.

--- SoundBoard/recorderViewController.swift
+++ SoundBoard/recorderViewController.swift
@@ -7,25 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
 class recorderViewController: UIViewController {
-
+    
     
     @IBOutlet var recordButton: UIView!
     @IBOutlet weak var textField: UITextField!
     
+    var audioRecorder : AVAudioRecorder?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupRecorder()
     }
-
+    
+    
+    func setupRecorder() {
+        
+        do{
+            
+            // create session
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            // create URL for the file
+            
+            let basePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponets = [basePath,"audio.m4a"]
+            let audioURL = NSURL.fileURL(withPathComponents: pathComponets)!
+            
+            // create settings for the audio recorder
+            
+            var settings : [String:AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject? as AnyObject
+            settings[AVSampleRateKey] = 44100.0 as AnyObject
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject
+            
+            
+            // create Audio Recorer Object
+            
+            audioRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
+            audioRecorder!.prepareToRecord()
+            
+        }catch let error as NSError {
+            print (error)
+            
+        }catch {}
+        
+        
+    }
+    
     @IBAction func addTapped(_ sender: Any) {
     }
     
     @IBAction func playTapped(_ sender: Any) {
     }
-
+    
     @IBAction func recordTapped(_ sender: Any) {
     }
 }
